e364c0835d550df3ca8d935b5a8fc6a80fe7661d
Marcia Alves <marciasoalves@gmail.com>
2022-07-07 13:45:31 -0300

    Projeto criado

---
+++ src/main/java/com/marcialves/worshopmongo/WorshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.marcialves.worshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorshopmongoApplication.class, args);
+	}
+
+}
