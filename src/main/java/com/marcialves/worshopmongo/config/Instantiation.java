ddd4cf528e0cd41e837cbc7f665ab3f1e8b7b47e
Marcia Alves <marciasoalves@gmail.com>
2022-07-07 21:37:54 -0300

    Operação de instanciação da base de dados

---
+++ src/main/java/com/marcialves/worshopmongo/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.marcialves.worshopmongo.config;
+
+import com.marcialves.worshopmongo.domain.User;
+import com.marcialves.worshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository userReposiroty;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userReposiroty.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
