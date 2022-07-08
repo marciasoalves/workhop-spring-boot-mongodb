9c4c2393134ad5ea8b5e37dc1b9d49fd9742fd12
Marcia Alves <marciasoalves@gmail.com>
2022-07-08 16:38:57 -0300

    Obtendo um usuário por id

--- src/main/java/com/marcialves/worshopmongo/services/UserService.java
+++ src/main/java/com/marcialves/worshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.marcialves.worshopmongo.services;
 
 import com.marcialves.worshopmongo.domain.User;
 import com.marcialves.worshopmongo.repository.UserRepository;
+import com.marcialves.worshopmongo.services.excepition.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +16,10 @@ public class UserService {
     private UserRepository repo;
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
